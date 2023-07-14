90a9b92c20e8111c2a8d7c53e16d541685aae9e0
Manzareka <mi.machitidze@gmail.com>
2023-07-14 18:24:17 +0400

    Add javascript
    Add javascript

---
+++ script.js
@@ -0,0 +1,48 @@
+// Number
+let myNumber = 30;
+console.log(myNumber);
+console.log(typeof myNumber);
+
+// String
+let myString = "This is a string";
+console.log(myString);
+console.log(typeof myString);
+
+// Boolean
+let myBoolean = true;
+console.log(myBoolean);
+console.log(typeof myBoolean);
+
+// Undefined
+let myUndefined;
+console.log(myUndefined);
+console.log(typeof myUndefined);
+
+// Null
+let myNull = null;
+console.log(myNull);
+console.log(typeof myNull);
+
+// Variables
+let myName = "Mikheil";
+let myHobby = "playing a piano";
+let yearOfBirth = 1990;
+let currentYear = 2023;
+let myAge = currentYear - yearOfBirth;
+
+// Syntaxes
+console.log(
+  `My name is ${myName}, I am ${
+    currentYear - yearOfBirth
+  } years old and my hobby is ${myHobby}.`
+);
+
+console.log(
+  "My name is " +
+    myName +
+    ", I am " +
+    myAge +
+    " years old and my hobby is " +
+    myHobby +
+    "."
+);
